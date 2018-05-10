5a9b4a51a37ec9c32aba47d7e9e0ef3f2b2353f2
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2018-05-10 17:39:43 +0200

    setting up file parsing function

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Finder\Finder;
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,27 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        $fileContents = array();
+        $finder = new Finder();
+        $finder->files()->in('files');
+        
+        foreach ($finder as $file) {
+            $contents = $file->getContents();
+            $this->parsePatientInfo($contents);
+            // $fileContents[] = $contents;
+        }
+
+        print_r($fileNames);
+        die;
+        // // replace this example code with whatever you need
+        // return $this->render('default/index.html.twig', array(
+        //     'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        // ));
+    }
+    
+    public function parsePatientInfo($string)
+    {
+        print_r($string);
+        die;
     }
 }
